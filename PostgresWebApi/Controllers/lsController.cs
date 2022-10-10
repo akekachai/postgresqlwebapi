883cd8ae3618b520eed6a927e35e8e3714ea6f7b
akekachai <akekachai.j@sabuytech.com>
2022-10-10 21:30:37 +0700

    v7

---
+++ PostgresWebApi/Controllers/lsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PostgresWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class lsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> getall()
+        {
+            return Ok(new { success = true, message = "" });
+        }
+    }
+}
